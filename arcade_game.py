ca21535ca66d758d8d7dcef3a10f49e4746de257
Brenton Williams <brentw263@gmail.com>
2013-06-11 10:58:27 -0500

    Main program loop
    updated main program loop.

--- arcade_game.py
+++ arcade_game.py
@@ -1,26 +1,27 @@
 import pygame
 
-pi = 3.141592653
+#Define some colors.
+black = (   0,   0,   0)
+white = ( 255, 255, 255)
+green = (   0, 255,   0)
+red   = ( 255,   0,   0)
 
 pygame.init()
 
-black = (0, 0, 0)
-white = (255, 255, 255)
-green = (0, 255, 0)
-red = (255, 0, 0)
-
+#Set the width and height of the screen.
 size = (700, 500)
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption("My awesome game!)
+
+pygame.display.set_caption("Scuda Bang!")
 
 #Loop until the user clicks the close button.
 done = False
 
 #Used to manage how fast the screen updates
-clock = pygame.time.clock()
+clock = pygame.time.Clock()
 
 #--------Main Program Loop--------
-while done = False:
+while done == False:
 
     #ALL EVENT PROCESSING SHOULD GO BELOW THIS COMMENT
     for event in pygame.event.get(): #User did something.                       
@@ -36,8 +37,23 @@ while done = False:
 
 
     #ALL CODE TO DRAW SHOULD GO BELOW THIS COMMENT.
+                           
+    #First, clear the screen to white. Don't put any other drawing commeands
+    #above this or they will be erased.
+    screen.fill(white)
 
+    for y_offset in range(0,100,10):
+        pygame.draw.line(screen,green,[0,10+y_offset],[100,110+y_offset],5)
+        
     #ALL CODE TO DRAW SHOULE GO ABOVE THIS COMMENT.
+
+    #Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
 
     #Limit to 20 frames per second.
     clock.tick(20)
+
+#CLose the window and quit.
+#If you forget this line, the program will hang.
+#on exit if running from IDLE.
+pygame.quit()
