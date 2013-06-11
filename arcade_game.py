ceb67681fff7430aa0f6bc07e620815f2c12b4ed
Brenton Williams <brentw263@gmail.com>
2013-06-11 00:30:59 -0500

    Python arcade game
    Added python arcade game script.

---
+++ arcade_game.py
@@ -0,0 +1,13 @@
+import pygame
+
+import math.pi
+
+pygame.init()
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+green = (0, 255, 0)
+red = (255, 0, 0)
+
+size = (700, 500)
+screen = pygame.display.set_mode(size)
